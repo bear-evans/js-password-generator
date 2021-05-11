c92e26852d00d648b1f782ee0f658a6a88869d7e
Bear Evans <81526068+bear-evans@users.noreply.github.com>
2021-05-10 21:30:19 -0400

    Added variables and function skeletons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,17 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Booleans for characters to include
+var genLower = false;
+var genUpper = false;
+var genNumbers = false;
+var genSymbols = false;
+var glyphPool = [];
+
+  // Check the checkbox parameters and fill the pool of potential glyphs
+function fillPool() {
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -9,6 +20,13 @@ function writePassword() {
   passwordText.value = password;
 
 }
+
+ // Actually use the pool to generate a password, using a for loop.
+ // Outputs a string.
+function generatePassword() {
+}
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
