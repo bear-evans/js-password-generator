6f8e300a4bc78f55e1fe43d8222d6d802c901601
Bear Evans <81526068+bear-evans@users.noreply.github.com>
2021-05-12 17:22:38 -0400

    added glyph pool query and filling, removed checkboxes

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Booleans for characters to include, and pool of characters
-// associated with them
-var genLower = false;
+// Glyph sets to be added to the pool based on user input
 var lowerGlyphs = [
   "a", "b", "c", "d",
   "e", "f", "g", "h",
@@ -14,7 +12,6 @@ var lowerGlyphs = [
   "y", "z"
 ];
 
-var genUpper = false;
 var upperGlyphs = [
   "A", "B", "C", "D",
   "E", "F", "G", "H",
@@ -25,14 +22,12 @@ var upperGlyphs = [
   "Y", "Z"
 ];
 
-var genNumbers = false;
 var numberGlyphs = [
   "0", "1", "2", "3",
   "4", "5", "6", "7",
   "8", "9"
 ];
 
-var genSymbols = false;
 var symbolGlyphs = [
   "@", "%", "+", "\\",
   "/", "'", "!", "#",
@@ -42,6 +37,7 @@ var symbolGlyphs = [
   "`", "-", "_", ".",
 ];
 
+// The blank glyph pool
 var glyphPool = [];
 
 // Check the checkbox parameters and fill the pool of potential glyphs
@@ -54,8 +50,28 @@ function generatePassword() {
 
 }
 
+function queryUser() {
+  // The glyph pool begins empty. As the user is queried for 
+  if (window.confirm("Would you like to use lowercase letters?")) {
+    glyphPool = glyphPool.concat(lowerGlyphs);
+  }
+  if (window.confirm("Would you like to use uppercase letters?")) {
+    glyphPool = glyphPool.concat(upperGlyphs);
+  }
+  if (window.confirm("Would you like to use numbers?")) {
+    glyphPool = glyphPool.concat(numberGlyphs);
+  }
+  if (window.confirm("Would you like to use special characters?")) {
+    glyphPool = glyphPool.concat(symbolGlyphs);
+  }
+
+  //console.log(glyphPool);
+
+}
+
 // Write password to the #password input
 function writePassword() {
+  queryUser();
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
