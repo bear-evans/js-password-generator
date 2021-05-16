a523305ffe7ff0e15d151a68e8cc5c1be1279f9c
brickdwarf <81526068+brickdwarf@users.noreply.github.com>
2021-05-15 20:24:52 -0400

    added screenshot and updated Readme

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# js-password-generator
+# Javascript Password Generator
+
+![screenshot](https://raw.githubusercontent.com/bear-evans/js-password-generator/main/assets/Preview.jpg)
+
+## About This Project
+
+This web app generates a secure, random password. It can be configured to include lowercase, uppercase, numerical, and special glyphs and can be of any length between 8 and 128, inclusive.
+
+## Built With
+
+This web app is built using HTML and CSS, with Javascript providing the generation.
+
+## Installation and Usage
+
+Clone the repository using the following command
+
+```
+git clone git@github.com:bear-evans/js-password-generator.git
+```
+
+You may also view a live demo of the application [here](https://bear-evans.github.io/js-password-generator/).
--- script.js
+++ script.js
@@ -12,46 +12,44 @@ var passLength = 0; // The password length
 // Write password to the #password input
 function writePassword() {
   queryUser();
-
-
-  if (glyphPool.length === 0) { // The user didn't specify any glyphs to use.
+  // If the user didn't specify any glyphs to use, abort.
+  if (glyphPool.length === 0) {
     window.alert("Unable to generate password: You must select at least one type of character to include.");
     return;
   }
-
-  if (passLength === null) { // The user canceled the input prompt.
+  // If the user canceled the input prompt, abort.
+  if (passLength === null) {
     return;
   }
-
+  // Generate the password based on the defined parameters.
   var password = generatePassword(glyphPool, passLength);
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
 
+// Assembles a pool based on user input.
 function queryUser() {
-
-  glyphPool = []; //  reset the glyph pool each time
-
+  //  reset the glyph pool each run
+  glyphPool = [];
+  // Query the user, and add the glyphs they want to the pool.
   if (window.confirm("Would you like to use lowercase letters?")) {
-    glyphPool = glyphPool.concat(Array.from('abcdefghijklmnopqrstuvwxyz'));
+    glyphPool = glyphPool.concat(Array.from("abcdefghijklmnopqrstuvwxyz"));
   }
   if (window.confirm("Would you like to use uppercase letters?")) {
-    glyphPool = glyphPool.concat(Array.from('ABCDEFGHIJKLMNOPQRSTUVWXYZ'));
+    glyphPool = glyphPool.concat(Array.from("ABCDEFGHIJKLMNOPQRSTUVWXYZ"));
   }
   if (window.confirm("Would you like to use numbers?")) {
-    glyphPool = glyphPool.concat(Array.from('1234567890'));
+    glyphPool = glyphPool.concat(Array.from("1234567890"));
   }
   if (window.confirm("Would you like to use special characters?")) {
-    glyphPool = glyphPool.concat(Array.from('!@#$%^(){}[]~`?-_.:/\\'));
+    glyphPool = glyphPool.concat(Array.from("!@#$%^(){}[]~`?-_.:/\\"));
   }
 
+  // Keep asking the user for input until they abort or give a valid number
   var isLengthSet = false;
-  
   while (isLengthSet === false) {
     setLength = window.prompt("How long should the password be? Choose a whole number between 8 and 128, inclusive.", 12);
-    console.log(passLength);
+    // Handle inappropriate input first, then accept.
     if (setLength === null) {
       return null;
     } else if(setLength < 8) {
@@ -61,26 +59,26 @@ function queryUser() {
     } else if (isNaN(setLength)) {
       window.alert("Invalid input. Password length must be an integer.");
     } else {
-      passLength = parseInt(setLength, 10);
+      passLength = parseInt(setLength, 10); // Normalizes the number in case of a decimal.
       isLengthSet = true;  
     }
   }
 }
 
-// Actually use the pool to generate a password, using a for loop.
-// Outputs a string.
+// Use pool to generate a password.
 function generatePassword(pool, length) {
   var pwd = [];
+
   for (var i = 0; i <= length; i++) {
     glyph = pool[getRandom(pool.length)];
     pwd.push(glyph);
   }
-
+  // Combines the array into a string, and sends it.
   pwd = pwd.join('');
-
   return pwd;
 }
 
+// Quick and dirty random number generation.
 function getRandom(max) {
   return Math.floor(Math.random() * max);
 }
